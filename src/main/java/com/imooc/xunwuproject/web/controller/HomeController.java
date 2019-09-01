af63a31d3b74b23d7715703385f065f55a4bb3b3
wuchubuzai2018 <2009longyu@163.com>
2019-09-01 16:42:00 +0800

    增加全局错误处理与全局API处理

--- src/main/java/com/imooc/xunwuproject/web/controller/HomeController.java
+++ src/main/java/com/imooc/xunwuproject/web/controller/HomeController.java
@@ -1,8 +1,10 @@
-package com.imooc.xunwuproject.controller;
+package com.imooc.xunwuproject.web.controller;
 
+import com.imooc.xunwuproject.base.ApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -11,10 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 
-    @GetMapping(name = "/")
+    @GetMapping("/")
     public String index(Model model){
         model.addAttribute("name","慕课时间");
         return "index";
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public ApiResponse get(){
+        return ApiResponse.ofMessage(200,"成功了");
     }
 
 
